f6f08b5983c29a8ff13101551be4f30861a64e46
sbswarna <Shatabdi.biswas@brainstation-23.com>
2025-05-11 20:10:46 +0600

    fix: Update heading text in PortfolioDetailsComponent for consistency

--- src/components/Portfolio/Details/index.tsx
+++ src/components/Portfolio/Details/index.tsx
@@ -24,7 +24,7 @@ const PortfolioDetailsComponent = async ({ portfolio }: PortfolioDetailsProps) =
         {portfolio.relatedProjects && (
           <>
             <hr className="mt-16" />
-            <h2 className="text-3xl font-bold my-10">Explore More Inspiring Work</h2>
+            <h2 className="text-3xl font-bold my-10">Explore More of Our Works</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
               {(portfolio.relatedProjects as Portfolio[])?.map((project) => (
                 <Card

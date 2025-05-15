e147f95ed25206fa5229f19512a0d841923af5cf
anika-khaled-bs <anika.khaled@brainstation-23.com>
2025-05-15 16:50:35 +0600

    Removed loaders

--- src/app/(frontend)/services/[slug]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { LoadingIndicator } from '@/components/ui/loading-indicator'
-
-export default function ServiceDetailLoading() {
-  return <LoadingIndicator message="Loading service details..." className="mt-16" />
-}
